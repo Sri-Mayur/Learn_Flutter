fdab7e58de8710bf493d388e9d070cd546f1adf0
Sri-Mayur <mayursrivastava877@gmail.com>
2024-10-15 21:20:09 +0530

    Learned about difference b/w final and const

---
+++ finalAndconst.dart
@@ -0,0 +1,44 @@
+//Final and const doesn't allow further changes in the variables
+
+
+
+void main() {
+
+  final String name = "Mayur";
+  const profile = "Flutter";
+
+  //name = "ABCD"; //An error will be shown here
+  //profile = 232 //Same error will be shown here
+
+  /* final String name = "XYZ" can be written
+    final var profile = "Developer" can't be written
+  */
+
+  final currentTime = DateTime.now(); // Runtime value
+  const pi = 3.14; // Compile-time constant
+
+  print(currentTime); // Outputs current time
+  print(pi); // Outputs 3.14
+
+
+
+
+
+
+
+
+  /**-------------------------------------------------------- */
+
+  final names1 = {
+    "Mayur",
+    "Srivastav",
+    "Flutter",
+    "Dart"
+  };
+
+  names1.add("Hello"); //We can add data with final variable but const variables can't do this.
+
+  //names1 = ["Helloo", "World"]; //final variables can only be set once.
+
+
+}
